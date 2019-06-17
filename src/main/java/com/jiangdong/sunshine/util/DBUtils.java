1d2184e6c3afda34f2d190d32397efda11d46774
jiangdong <jiangdonghpu@163.com>
2019-06-17 08:41:20 +0800

    添加DBUtils相关方法

--- src/main/java/com/jiangdong/sunshine/util/DBUtils.java
+++ src/main/java/com/jiangdong/sunshine/util/DBUtils.java
@@ -3,18 +3,32 @@ package com.jiangdong.sunshine.util;
 import com.jiangdong.sunshine.config.DBInit;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBUtils {
 
     private static DBInit dbInit = DBInit.getDBInit();
 
+    public static Connection getConnection() {
+        return dbInit.getConnection();
+    }
+
     public static void closeConnection(Connection connection) throws SQLException {
         connection.close();
     }
 
-    public static Connection getConnection() {
-        return dbInit.getConnection();
+    public static void closeResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet != null) {
+            resultSet.close();
+        }
+    }
+
+    public static void closeStatement(Statement statement) throws SQLException {
+        if (statement != null) {
+            statement.close();
+        }
     }
 
 }
