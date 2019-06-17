37cdaadea64263869a4a09c82705a5950a1e19f9
jiangdong <jiangdonghpu@163.com>
2019-06-17 09:55:10 +0800

    分支测试

--- src/main/java/com/jiangdong/sunshine/factory/BaseFactory.java
+++ src/main/java/com/jiangdong/sunshine/factory/BaseFactory.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * \
+ */
 public class BaseFactory {
 
     private static SqlOperation executeRunner = SqlOperationFactory.getOperation(ExecuteRunner.class);
