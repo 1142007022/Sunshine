bc70d294881396342e31931e05d832ec225ea17c
jiangdong <jiangdonghpu@163.com>
2019-06-21 11:37:11 +0800

    添加新的工具类

---
+++ src/main/java/com/jiangdong/sunshine/util/FileUtils.java
@@ -0,0 +1,31 @@
+package com.jiangdong.sunshine.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+
+    /**
+     * @param fileName 文件名
+     * @param str      统计的字符串
+     * @return         统计一个文件中某个字符串出现的次数
+     * @throws Exception
+     */
+    public static int getCount(String fileName, String str) throws Exception {
+        int count = 0;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder = stringBuilder.append(line);
+        }
+        int a = 0;
+        while ((a = stringBuilder.indexOf(str)) != -1) {
+            stringBuilder = stringBuilder.delete(a, a + str.length());
+            count++;
+        }
+        return count;
+    }
+
+}
