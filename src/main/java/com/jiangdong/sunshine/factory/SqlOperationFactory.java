c1ec73e65b1a660ee27f3a571262172315e23591
jiangdong <jiangdonghpu@163.com>
2019-06-24 16:45:20 +0800

    修复无返回ExecuteBatchRunner的bug

--- src/main/java/com/jiangdong/sunshine/factory/SqlOperationFactory.java
+++ src/main/java/com/jiangdong/sunshine/factory/SqlOperationFactory.java
@@ -1,5 +1,6 @@
 package com.jiangdong.sunshine.factory;
 
+import com.jiangdong.sunshine.runner.ExecuteBatchRunner;
 import com.jiangdong.sunshine.runner.ExecuteRunner;
 import com.jiangdong.sunshine.runner.QueryRunner;
 import com.jiangdong.sunshine.sql.SqlOperation;
@@ -8,24 +9,28 @@ public class SqlOperationFactory {
 
     private static ExecuteRunner executeRunner;
     private static QueryRunner queryRunner;
+    private static ExecuteBatchRunner executeBatchRunner;
 
     public static SqlOperation getOperation(Class clazz) {
         if (clazz.equals(ExecuteRunner.class)) {
-            if (executeRunner != null) {
-                return executeRunner;
-            } else {
+            if (executeRunner == null) {
                 executeRunner = new ExecuteRunner();
-                return executeRunner;
             }
+            return executeRunner;
         }
         if (clazz.equals(QueryRunner.class)) {
-            if (queryRunner != null) {
-                return queryRunner;
-            } else {
+            if (queryRunner == null){
                 queryRunner = new QueryRunner();
-                return queryRunner;
             }
+            return queryRunner;
         }
+        if (clazz.equals(ExecuteBatchRunner.class)) {
+            if (executeBatchRunner == null) {
+                executeBatchRunner = new ExecuteBatchRunner();
+            }
+            return executeBatchRunner;
+        }
+
         return null;
     }
 
