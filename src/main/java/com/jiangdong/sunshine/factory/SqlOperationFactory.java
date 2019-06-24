efd595803d08a34d9eebef27494b738a79ca533d
jiangdong <jiangdonghpu@163.com>
2019-06-24 16:55:06 +0800

    修复获取操作Runner可能出现的线程安全问题

--- src/main/java/com/jiangdong/sunshine/factory/SqlOperationFactory.java
+++ src/main/java/com/jiangdong/sunshine/factory/SqlOperationFactory.java
@@ -5,32 +5,43 @@ import com.jiangdong.sunshine.runner.ExecuteRunner;
 import com.jiangdong.sunshine.runner.QueryRunner;
 import com.jiangdong.sunshine.sql.SqlOperation;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SqlOperationFactory {
 
     private static ExecuteRunner executeRunner;
     private static QueryRunner queryRunner;
     private static ExecuteBatchRunner executeBatchRunner;
+    private static Lock lock = new ReentrantLock();
 
     public static SqlOperation getOperation(Class clazz) {
-        if (clazz.equals(ExecuteRunner.class)) {
-            if (executeRunner == null) {
-                executeRunner = new ExecuteRunner();
+        lock.lock();
+        try {
+            if (clazz.equals(ExecuteRunner.class)) {
+                if (executeRunner == null) {
+                    executeRunner = new ExecuteRunner();
+                }
+                return executeRunner;
             }
-            return executeRunner;
-        }
-        if (clazz.equals(QueryRunner.class)) {
-            if (queryRunner == null){
-                queryRunner = new QueryRunner();
+            if (clazz.equals(QueryRunner.class)) {
+                if (queryRunner == null) {
+                    queryRunner = new QueryRunner();
+                }
+                return queryRunner;
             }
-            return queryRunner;
-        }
-        if (clazz.equals(ExecuteBatchRunner.class)) {
-            if (executeBatchRunner == null) {
-                executeBatchRunner = new ExecuteBatchRunner();
+            if (clazz.equals(ExecuteBatchRunner.class)) {
+                if (executeBatchRunner == null) {
+                    executeBatchRunner = new ExecuteBatchRunner();
+                }
+                return executeBatchRunner;
             }
-            return executeBatchRunner;
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            lock.unlock();
         }
-
+       
         return null;
     }
 
