d57947c82f60dcf4ac1a9d160278ecfea89ef850
jiangdong <jiangdonghpu@163.com>
2019-07-05 11:57:25 +0800

    添加缓存工具类

---
+++ src/main/java/com/jiangdong/sunshine/cache/CacheManager.java
@@ -0,0 +1,81 @@
+package com.jiangdong.sunshine.cache;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @program: Sunshine
+ * @description: 缓存工具类 可根据
+ * @author: JD
+ * @create: 2019-07-05 11:32
+ **/
+public class CacheManager {
+
+    /**
+     * 设置过期时间若为0 则不过期
+     */
+    private static Long NO_TIME = 0L;
+
+    /**
+     * 键值对集合
+     */
+    private final static Map<String, Entity> map = new ConcurrentHashMap<>();
+    /**
+     * 定时器线程池，用于清除过期缓存
+     */
+    private final static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    public synchronized static void put(String key, Object data) {
+        CacheManager.put(key, data, NO_TIME);
+    }
+
+    public static void put(String key, Object data, long time) {
+        CacheManager.remove(key);
+        if (time > 0) {
+            Future future = executor.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    map.remove(key);
+                }
+            }, time, TimeUnit.MILLISECONDS);
+            map.put(key, new Entity(data, future));
+        } else {
+            map.put(key, new Entity(data, null));
+        }
+    }
+
+    public static <T> T get(String key) {
+        Entity entity = map.get(key);
+        return entity == null ? null : (T) entity.value;
+    }
+
+    public static <T> T remove(String key) {
+        //清除原缓存数据
+        Entity entity = map.remove(key);
+        if (entity == null) {
+            return null;
+        }
+        //清除原键值对定时器
+        if (entity.future != null) {
+            entity.future.cancel(true);
+        }
+        return (T) entity.value;
+    }
+
+    public static Integer size() {
+        return map.size();
+    }
+
+    private static class Entity {
+
+        public Object value;
+
+        public Future future;
+
+        public Entity(Object value, Future future) {
+            this.value = value;
+            this.future = future;
+        }
+    }
+
+}
