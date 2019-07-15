3bc2ea5ec7f124d39efc5aaed9615bed08d9ff31
jiang <1142007022@qq.com>
2019-07-15 19:20:38 +0800

    添加rollBackFor属性 可自定义异常的class进行事务回滚

--- src/main/java/com/jiangdong/sunshine/annotation/Rollback.java
+++ src/main/java/com/jiangdong/sunshine/annotation/Rollback.java
@@ -2,10 +2,15 @@ package com.jiangdong.sunshine.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 2019.7.15添加rollBackFor属性 可自定义异常的class进行事务回滚
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @Inherited
 @Documented
 public @interface Rollback {
+
+    Class rollBackFor() default Exception.class;
 
 }
