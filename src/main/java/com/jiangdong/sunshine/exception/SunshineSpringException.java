fc0c05d1e00ba9fe71a6dda4f3e7c3e84993e17b
jiangdong <1142007022@qq.com>
2019-08-24 10:34:45 +0800

    添加和spring整合的相关方法

---
+++ src/main/java/com/jiangdong/sunshine/exception/SunshineSpringException.java
@@ -0,0 +1,17 @@
+package com.jiangdong.sunshine.exception;
+
+public class SunshineSpringException extends SunShineBaseException {
+
+    public SunshineSpringException() {
+
+    }
+
+    public SunshineSpringException(String message) {
+        super(message);
+    }
+
+    public SunshineSpringException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
