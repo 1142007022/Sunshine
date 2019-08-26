387b8d99833a598593e7c5f4548259f511b80d32
jiangdong <1142007022@qq.com>
2019-08-26 14:09:33 +0800

    重写依赖注入方法

--- src/main/java/com/jiangdong/sunshine/spring/SunshineSpringDI.java
+++ src/main/java/com/jiangdong/sunshine/spring/SunshineSpringDI.java
@@ -1,9 +1,18 @@
 package com.jiangdong.sunshine.spring;
 
+import java.util.Iterator;
+
 public class SunshineSpringDI extends SunShineSpringContext {
 
     @Override
     public Object getBean(Class clazz) {
+        Iterator<Class<?>> classIterator =  classes.iterator();
+        while (classIterator.hasNext()){
+            Class c = classIterator.next();
+            if (c != null && c.equals(clazz)) {
+                return c;
+            }
+        }
         return null;
     }
 
