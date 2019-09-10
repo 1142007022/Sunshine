55686b4882c2361183f0df859ba4f318a63b832b
jiangdong <1142007022@qq.com>
2019-09-10 16:32:37 +0800

    删除无用的排序方法

--- src/main/java/com/jiangdong/sunshine/util/CollectionUtils.java
+++ src/main/java/com/jiangdong/sunshine/util/CollectionUtils.java
@@ -24,17 +24,4 @@ public class CollectionUtils {
         return !isEmpty(collection);
     }
 
-    public static int[] formatArray(int[] num) {
-        for (int i = 0; i < num.length - 1; i++) {
-            for (int j = 0; j < num.length - i - 1; j++) {
-                if (num[j] > num[j + 1]) {
-                    int temp = num[j];
-                    num[j] = num[j + 1];
-                    num[j + 1] = temp;
-                }
-            }
-        }
-        return num;
-    }
-
 }
