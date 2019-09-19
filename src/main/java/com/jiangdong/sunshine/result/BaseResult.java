5636ca6b00c7e9116b1fea3803c2ee4728a6d705
jiangdong <1142007022@qq.com>
2019-09-19 11:49:44 +0800

    添加抽象方法

--- src/main/java/com/jiangdong/sunshine/result/BaseResult.java
+++ src/main/java/com/jiangdong/sunshine/result/BaseResult.java
@@ -1,9 +1,12 @@
 package com.jiangdong.sunshine.result;
 
 import java.util.List;
+import java.util.Map;
 
 public abstract class BaseResult {
 
     abstract List<String> getParam();
+
+    abstract Map<String,Object> getParamAndValue();
 
 }
